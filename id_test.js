713c1e8f41577a95557af0da32fcb74ceb316333
yfeng392 <yfeng392@gatech.edu>
2025-07-31 15:11:22 -0700

    Modify id_test

--- id_test.js
+++ id_test.js
@@ -1,17 +1,20 @@
 const dfMessenger = document.querySelector('df-messenger');
 
+const metadata = {
+  "subscription plan": "Business Premium Plus",
+  "user_id": "12345",
+  "user_type": "admin"
+};
+
 dfMessenger.addEventListener('df-messenger-loaded', function () {
-  const userId = "12345";
-  if (userId) {
-    dfMessenger.setContext({
-      name: 'user_info',
-      parameters: {
-        user_id: userId
-      },
-      lifespan: 1
-    });
-    console.log(`User ID '${userId}' sent to Dialogflow Messenger context.`);
+  if (dfMessenger) {
+      dfMessenger.setContext({
+          name: 'user_metadata',
+          parameters: metadata,
+          lifespan: 1 
+      });
+      console.log(`Metadata for user ID '${metadata.user_id}' sent to Dialogflow Messenger context.`);
   } else {
-    console.log('User ID not found or not logged in.');
+      console.log('df-messenger element not found on the page.');
   }
 });
